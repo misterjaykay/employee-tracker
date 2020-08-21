7651f3f47ffc0a39ff29b74d195587722a9345bf
Jonghyun Kang <misterjaykay@gmail.com>
2020-08-20 20:49:16 -0400

    added prompt questions

--- index.js
+++ index.js
@@ -16,5 +16,107 @@ var connection = mysql.createConnection({
     database: "playlist_db",
   });
 
-  //questions
+//questions
+const mainPrmpt = {
+    type: "list",
+    name: "main",
+    message: "What would you like to do?",
+    choices: [
+    "View All Employees",
+    "View All Employees by Department",
+    "View All Employees by Manager",
+    "Add Employee",
+    "Remove Employee",
+    "Update Employee Role",
+    "Update Employee Manager"
+    ]
+};
 
+const addInfo = [
+    {
+        type: "input",
+        name: "main",
+        message: "What is the employee's first name??"
+    },
+    {
+        type: "input",
+        name: "main",
+        message: "What is the employee's last name?"
+    },
+    {
+        type: "input",
+        name: "main",
+        message: "What is the employee's role?"
+    },
+    {
+        type: "input",
+        name: "main",
+        message: "Who is the employee's manager?"
+    }
+];
+
+const askRemove = {
+    type: "list",
+    name: "main",
+    message: "Which employee do you want to remove?",
+    choices: ["LIST OF EMPLOYEES HERE"]
+}
+
+const askRole = {
+    type: "list",
+    name: "main",
+    message: "Which employee do you want to remove?",
+    choices: [
+        "Sales Lead",
+        "Salesperson",
+        "Lead Engineer",
+        "Software Engineer",
+        "Account Manager",
+        "Accountant",
+        "Legal Team Lead",
+        "Lawyer"
+    ]
+}
+
+// List of department
+// Sales Engineering Finance Legal
+
+function init() {
+    inquirer
+    .prompt(mainPrmpt)
+    .then(function(res) {
+        console.log('results',res);
+        switch(res) {
+            case "View All Employees":
+                // function here
+                break;
+            case "View All Employees by Department":
+                // function
+                break;
+            case "View All Employees by Manager":
+                // function
+                break;
+            case "Add Employee":
+                // function
+                break;
+            case "Remove Employee":
+                // function
+                break;
+            case "Update Employee Role":
+                // function
+                break;
+            case "Update Employee Manager":
+                // function
+                break;
+        }
+    });
+}
+
+init();
+
+  
+  
+  
+  
+
+    
