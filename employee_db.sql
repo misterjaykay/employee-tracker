a3dba2278229ffcbbfea24b98ceda09d16ad66b9
Jonghyun Kang <misterjaykay@gmail.com>
2020-08-20 20:28:43 -0400

    created a three tables employee,dept, and role

---
+++ employee_db.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) default 0,
+    department_id INT
+);
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+INSERT INTO
+VALUES
+
+SELECT * FROM employee;
--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+const { lavender } = require("color-name");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "password",
+    database: "playlist_db",
+  });
+
+  //questions
 
